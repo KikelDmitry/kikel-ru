c507da961becd11bf03688d85615f73d9576f9ba
Kikel Dmitry <kikeldmitry@gmail.com>
2026-08-17 16:53:19 +0300

    init

---
+++ config/category.js
@@ -0,0 +1,14 @@
+export const genres = {
+	seascape: 'Морской пейзаж',
+	stilllife: 'Натюрморт',
+	paysage: 'Пейзаж',
+	subject: 'Сюжет',
+	portrait: 'Портрет',
+	italy: 'Итальянский пейзаж',
+};
+
+export const technique = {
+	hm: 'Холст, масло',
+	plm: 'Панель, левкас, масло',
+	km: 'Картон, масло'
+}
---
+++ server/gapi.js
@@ -0,0 +1,9 @@
+import { google } from "googleapis";
+
+export const auth = new google.auth.JWT({
+	keyFile: "service-account.json",
+	scopes: [
+		"https://www.googleapis.com/auth/drive.readonly",
+		"https://www.googleapis.com/auth/spreadsheets.readonly",
+	],
+});
